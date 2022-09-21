7d63ec50d3e68da7a125998a556e4527120d5b69
serratusakiphat <devthree@scipiomundine.com>
2022-09-21 10:44:22 -0700

    first commit

---
+++ appscipy/src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+  const mystyle = {
+    color: "white",
+    fontSize: "20px",
+    fontFamily: "arial",
+  };
+
+  const nameBucket = "Scipio";
+  const myAge = 28;
+  const message =
+    "My name is " + nameBucket + " and I am " + myAge + " years young.";
+  // console.log(message);
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>My count is {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Add Count</button>
+      <p style={mystyle}>{message}</p>
+      {/* // We set up our react js application from scratch and also, deleted */}
+      {/* unwanted files // jsx practice and implementing variables and styles // */}
+      {/* whatever goes in curly braces {} is Javascript // styles is a attribute */}
+      {/* and could be used in javascript // we created object to style. We used a */}
+      {/* key, value pairs */}
+    </div>
+  );
+}
+
+export default App;
