c97dcc414d697e54d4440c120a17d4e285e47b2d
serratusakiphat <devthree@scipiomundine.com>
2022-09-22 07:57:19 -0700

    manually created fake data info for profile cards

---
+++ appscipy/src/mockDatabase.js
@@ -0,0 +1,16 @@
+const profiles = [
+    {
+        name: "KawZie NoZi",
+        image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstreetartnews.net%2Fwp-content%2Fuploads%2F2019%2F09%2Fdeansunshine_landofsunshine_melbourne_streetart_graffiti_streetartnews_KAWS-Melbourne-NGV-19.jpg&f=1&nofb=1",
+        title: "Founder & CEO",
+        description: "Description: I am a huge KAWS collector 🚀. I am also a developer 💻.", 
+    },
+    {
+        name: "LaDy KawZie",
+        image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstreetartnews.net%2Fwp-content%2Fuploads%2F2019%2F09%2Fdeansunshine_landofsunshine_melbourne_streetart_graffiti_streetartnews_KAWS-Melbourne-NGV-16.jpg&f=1&nofb=1",
+        title: "Co-Founder & Chief Evangelist",
+        description: "Description: I am a designer and part-time head honcho 🧑‍🚀.", 
+    }
+]
+
+export default profiles
