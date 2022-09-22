47e3c785f062caf011993698ada7a85b8a111242
Scipio Mundine jr <100849803+serratusakiphat@users.noreply.github.com>
2022-09-21 21:10:41 -0700

    note to our state variable

--- appscipy/src/App.js
+++ appscipy/src/App.js
@@ -16,9 +16,11 @@ function App() {
   // console.log(message);
 
   const [count, setCount] = useState(0);
+  // increment and decrement a state variable
+  // what we put in the state variable is default. Here we are starting at 0
 
   return (
-    <div>
+    <>
       <h1>My count is {count}</h1>
       <button onClick={() => setCount(count + 1)}>Add Count</button>
       <button onClick={() => setCount(count - 1)}>Minus Count</button>
@@ -28,7 +30,7 @@ function App() {
       {/* whatever goes in curly braces {} is Javascript // styles is a attribute */}
       {/* and could be used in javascript // we created object to style. We used a */}
       {/* key, value pairs! */}
-    </div>
+    </>
   );
 }
 
