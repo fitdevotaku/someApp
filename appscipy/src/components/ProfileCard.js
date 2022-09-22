69e570c000833dbf934adbec29c433ef752e5733
serratusakiphat <devthree@scipiomundine.com>
2022-09-22 07:31:16 -0700

    props updated

--- appscipy/src/components/ProfileCard.js
+++ appscipy/src/components/ProfileCard.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const ProfileCard = ({title, tagline, description}) => {
+const ProfileCard = ({image, name, title, description}) => {
     return (
       <div style={{backgroundColor: 'white', margin: '40px'}}>
         <div style={{display: 'flex'}}>
-                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstreetartnews.net%2Fwp-content%2Fuploads%2F2019%2F09%2Fdeansunshine_landofsunshine_melbourne_streetart_graffiti_streetartnews_KAWS-Melbourne-NGV-19.jpg&f=1&nofb=1"
+                <img src={image}
                     alt=""
                     height="300px"
                 />
             <div>
-                <h2 style={{ marginLeft: '10px', fontSize: '50px' }}>{title}</h2>
-                <h6 style={{ marginLeft: '10px', fontSize: '25px' }}>{tagline}</h6>
+                <h2 style={{ marginLeft: '10px', fontSize: '50px' }}>{name}</h2>
+                <h6 style={{ marginLeft: '10px', fontSize: '25px' }}>{title}</h6>
             </div>
         </div>
             
