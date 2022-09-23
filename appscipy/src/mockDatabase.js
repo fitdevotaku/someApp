e80fa4a819871b4e8d9172fcf46854e0f1e0a468
serratusakiphat <devthree@scipiomundine.com>
2022-09-22 22:49:56 -0700

    fake data base - updates

--- appscipy/src/mockDatabase.js
+++ appscipy/src/mockDatabase.js
@@ -6,7 +6,7 @@ const profiles = [
         description: "Description: I am a huge KAWS collector 🚀. I am also a developer 💻.", 
     },
     {
-        name: "LaDy KawZie",
+        name: "jR KawZie",
         image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstreetartnews.net%2Fwp-content%2Fuploads%2F2019%2F09%2Fdeansunshine_landofsunshine_melbourne_streetart_graffiti_streetartnews_KAWS-Melbourne-NGV-16.jpg&f=1&nofb=1",
         title: "Co-Founder & Chief Evangelist",
         description: "Description: I am a designer and part-time head honcho 🧑‍🚀.", 
